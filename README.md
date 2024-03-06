10f0ffb4527c889c1c0940fd7a9d02988278b7d2
Kunal-khanwalkar <kunalkhanwalkar@gmail.com>
2024-03-06 23:15:39 +0530

    Add laziness script

---
+++ README.md
@@ -0,0 +1,5 @@
+# CMake-Init
+
+This is a project initialization script for a build system generator responsible for creating a build file which helps in compiling and linking CXX libraries.
+
+(Quote React philosophy)
---
+++ builder.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+cat << "EOF"
+
+  ____ __  __       _          ___       _ _   
+ / ___|  \/  | __ _| | _____  |_ _|_ __ (_) |_ 
+| |   | |\/| |/ _` | |/ / _ \  | || '_ \| | __|
+| |___| |  | | (_| |   <  __/  | || | | | | |_ 
+ \____|_|  |_|\__,_|_|\_\___| |___|_| |_|_|\__|
+
+EOF
+
+echo "Let's initialize your CMake project!\n"
+
+read -p "Project name: " PROJECT_NAME && PROJECT_NAME=$(echo "${PROJECT_NAME}" | tr ' ' '-')
+read -p "CMake version (Default - 3.0.0): " CMAKE_VERSION && CMAKE_VERSION=${CMAKE_VERSION:-3.0.0}
+read -p "C++ Standard (Default - gnu++99): " CXX_STANDARD && CXX_STANDARD=${CXX_STANDARD:-99}
+read -p "Project ingress point (Default - main.cpp): " INGRESS_POINT && INGRESS_POINT=${INGRESS_POINT:-main.cpp}
+
+mkdir -p src
+mkdir -p out
+touch ${INGRESS_POINT}
+
+# Creating your CMakeLists.txt
+cat << EOF > CMakeLists.txt
+cmake_minimum_required(VERSION ${CMAKE_VERSION})
+
+project(${PROJECT_NAME})
+
+set (CMAKE_CXX_STANDARD ${CXX_STANDARD})
+set (CMAKE_RUNTIME_OUTPUT_DIRECTORY "out/")
+add_executable(${PROJECT_NAME} "${INGRESS_POINT}")
+target_include_directories(${PROJECT_NAME} PUBLIC "\${CMAKE_CURRENT_SOURCE_DIR}/include/")
+target_sources(${PROJECT_NAME} PRIVATE "\${CMAKE_CURRENT_SOURCE_DIR}/src/")
+EOF
+
+echo "\nCMakeLists.txt generated!"
+
+# gitignore for ignoring the CMake generated files
+cat << EOF > .gitignore
+CMakeFiles/
+CMakeCache.txt
+cmake_install.cmake
+Makefile
+out/
+EOF
+
+echo ".gitignore also created to manage the clutter (Give headpats! :])!"
